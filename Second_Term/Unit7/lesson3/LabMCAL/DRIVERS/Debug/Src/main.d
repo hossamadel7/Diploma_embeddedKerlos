c66808c7253a31713b91ea1acd4dc517a919ece2
hossamadel7 <57440281+hossamadel7@users.noreply.github.com>
2023-09-14 14:30:23 +0300

    Add files via upload

---
+++ Second_Term/Unit7/lesson3/LabMCAL/DRIVERS/Debug/Src/main.d
@@ -0,0 +1,10 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/KH/STM32CubeIDE/DRIVERS/stm32_f103c6_drivers/inc/stm32f103x6.h \
+ C:/Users/KH/STM32CubeIDE/DRIVERS/stm32_f103c6_drivers/inc/stm32_f103c6_GPIO.h \
+ C:/Users/KH/STM32CubeIDE/DRIVERS/stm32_f103c6_drivers/inc/stm32f103x6.h
+
+C:/Users/KH/STM32CubeIDE/DRIVERS/stm32_f103c6_drivers/inc/stm32f103x6.h:
+
+C:/Users/KH/STM32CubeIDE/DRIVERS/stm32_f103c6_drivers/inc/stm32_f103c6_GPIO.h:
+
+C:/Users/KH/STM32CubeIDE/DRIVERS/stm32_f103c6_drivers/inc/stm32f103x6.h:
---
+++ Second_Term/Unit7/lesson3/LabMCAL/DRIVERS/Debug/stm32_f103c6_drivers/subdir.mk
@@ -0,0 +1,19 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../stm32_f103c6_drivers/stm32_f103c6_GPIO.c 
+
+OBJS += \
+./stm32_f103c6_drivers/stm32_f103c6_GPIO.o 
+
+C_DEPS += \
+./stm32_f103c6_drivers/stm32_f103c6_GPIO.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+stm32_f103c6_drivers/stm32_f103c6_GPIO.o: ../stm32_f103c6_drivers/stm32_f103c6_GPIO.c
+	arm-none-eabi-gcc -gdwarf-2 "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DSTM32 -DSTM32F1 -DSTM32F103C6Tx -DDEBUG -c -I"C:/Users/KH/STM32CubeIDE/DRIVERS/stm32_f103c6_drivers/inc" -I../Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"stm32_f103c6_drivers/stm32_f103c6_GPIO.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
